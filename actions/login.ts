8008f7abf39878d5e66df5b6beeb1870cebba200
cswebdev <csnider91@live.com>
2024-12-06 11:31:33 -0500

    fixed issue with server actions not displaying in terminal

--- actions/login.ts
+++ actions/login.ts
@@ -1,11 +1,26 @@
-"user server";
-import { z } from "zod";
-import { LoginSchema } from "@/schemas";
+"use server";
 
-type LoginValues = z.infer<typeof LoginSchema>;
+// import { z } from "zod";
+// import { LoginSchema } from "@/schemas";
 
-export const login = (values: LoginValues) => {
-  console.log("The Login function has been called on the server side");
-  console.log(values);
-  return values;
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export const login = async (values: any) => {
+   console.log(values);
 };
+
+// Define type for login values
+// type LoginValues = z.infer<typeof LoginSchema>;
+
+// export const login = async (values: LoginValues) => {
+//    console.log("Server Action: Login called");
+//    console.log("Received values:", values);
+
+//    // Simulate authentication logic
+//    if (values.email === "test@test.com" && values.password === "password") {
+//       console.log("Authentication successful!");
+//       return { success: true };
+//    } else {
+//       console.log("Authentication failed!");
+//       return { success: false, message: "Invalid credentials" };
+//    }
+// };
--- components/auth/login-form.tsx
+++ components/auth/login-form.tsx
@@ -9,12 +9,12 @@ import { Input } from "../ui/input";
 import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import {
-  Form,
-  FormControl,
-  FormField,
-  FormItem,
-  FormLabel,
-  FormMessage,
+   Form,
+   FormControl,
+   FormField,
+   FormItem,
+   FormLabel,
+   FormMessage,
 } from "@/components/ui/form";
 import { MdOutlineMailOutline } from "react-icons/md";
 import { RiLockPasswordLine } from "react-icons/ri";
@@ -22,90 +22,104 @@ import { Button } from "../ui/button";
 import { FormError } from "../form-error";
 import { FormSuccess } from "../form-success";
 import { login } from "@/actions/login";
+import { useTransition } from "react";
 
 const LoginForm = () => {
-  const form = useForm<z.infer<typeof LoginSchema>>({
-    resolver: zodResolver(LoginSchema),
-    defaultValues: {
-      email: "",
-      password: "",
-    },
-  });
+   const [isPending, startTransition] = useTransition();
 
-  const onSubmit = (values: z.infer<typeof LoginSchema>) => {
-    console.log("Form submitted with values:", values); // Should appear in the browser console
-    login(values); // Should trigger the server-side function
-  };
+   const form = useForm<z.infer<typeof LoginSchema>>({
+      resolver: zodResolver(LoginSchema),
+      defaultValues: {
+         email: "",
+         password: "",
+      },
+   });
 
-  return (
-    <CardWrapper
-      headerLabel="Welcome back"
-      backButtonLabel="Don't have an account?"
-      backButtonHref="/auth/register"
-      showSocial
-    >
-      <Form {...form}>
-        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6">
-          <div className="space-y-4">
-            <FormField
-              control={form.control}
-              name="email"
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel>Email</FormLabel>
-                  <FormControl>
-                    <div className="relative">
-                      <Input
-                        {...field}
-                        placeholder="my.email@email.com"
-                        className="pr-10" // Add padding to the right to avoid overlapping with the icon
-                      />
-                      <MdOutlineMailOutline
-                        className="absolute right-3 top-1/2 transform -translate-y-1/2 text-gray-400"
-                        size={20}
-                      />
-                    </div>
-                  </FormControl>
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
+   const onSubmit = async (values: z.infer<typeof LoginSchema>) => {
+      startTransition(async () => {
+         login(values);
+      });
+      // Should appear in the browser console
+      console.log("Form submitted with values:", values);
+      // Should trigger the server-side function
+      // login(values);
+   };
 
-            <FormField
-              control={form.control}
-              name="password"
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel>Password</FormLabel>
-                  <FormControl>
-                    <div className="relative">
-                      <Input
-                        {...field}
-                        placeholder="******"
-                        type="password"
-                        className="pr-10"
-                      />
-                      <RiLockPasswordLine
-                        className="absolute right-3 top-1/2 transform -translate-y-1/2 text-gray-400"
-                        size={20}
-                      />
-                    </div>
-                  </FormControl>
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
-          </div>
-          <FormError message="" />
-          <FormSuccess message="" />
+   //  const onSubmit = (values: z.infer<typeof LoginSchema>) => {
+   //     login(values);
+   //  };
 
-          <Button type="submit" className="w-full">
-            Login
-          </Button>
-        </form>
-      </Form>
-    </CardWrapper>
-  );
+   return (
+      <CardWrapper
+         headerLabel="Welcome back"
+         backButtonLabel="Don't have an account?"
+         backButtonHref="/auth/register"
+         showSocial
+      >
+         <Form {...form}>
+            <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6">
+               <div className="space-y-4">
+                  <FormField
+                     control={form.control}
+                     name="email"
+                     render={({ field }) => (
+                        <FormItem>
+                           <FormLabel>Email</FormLabel>
+                           <FormControl>
+                              <div className="relative">
+                                 <Input
+                                    {...field}
+                                    disabled={isPending}
+                                    placeholder="my.email@email.com"
+                                    className="pr-10" // Add padding to the right to avoid overlapping with the icon
+                                 />
+                                 <MdOutlineMailOutline
+                                    className="absolute right-3 top-1/2 transform -translate-y-1/2 text-gray-400"
+                                    size={20}
+                                 />
+                              </div>
+                           </FormControl>
+                           <FormMessage />
+                        </FormItem>
+                     )}
+                  />
+
+                  <FormField
+                     control={form.control}
+                     name="password"
+                     render={({ field }) => (
+                        <FormItem>
+                           <FormLabel>Password</FormLabel>
+                           <FormControl>
+                              <div className="relative">
+                                 <Input
+                                    {...field}
+                                    disabled={isPending}
+                                    placeholder="******"
+                                    type="password"
+                                    className="pr-10"
+                                 />
+                                 <RiLockPasswordLine
+                                    className="absolute right-3 top-1/2 transform -translate-y-1/2 text-gray-400"
+                                    size={20}
+                                 />
+                              </div>
+                           </FormControl>
+                           <FormMessage />
+                        </FormItem>
+                     )}
+                  />
+               </div>
+               <FormError message="" />
+               <FormSuccess message="" />
+
+               <Button disabled={isPending} type="submit" className="w-full">
+                  Login
+               </Button>
+            </form>
+         </Form>
+      </CardWrapper>
+   );
 };
 
 export default LoginForm;
